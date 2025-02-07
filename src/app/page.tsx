0a82110bf5f9726ad661577a1f7bfdd750b8eeb4
Hareem Farooqi <007hareem@gmail.com>
2025-02-07 21:04:16 +0500

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,24 +1,24 @@
 "use client"
 import React from 'react'
 import Image from "next/image";
-import FoodCategory from "../../components/FoodCategory";
-import HeroAboutus from '../../components/HeroAboutus';
-import HomePageExperience from '../../components/HomePageExperience';
-import ChefOnHome from '../../components/ChefOnHome';
-import SimilarProductsSection from 'components/SimilarProducts';
+import dynamic from 'next/dynamic';
+
+const HeroAboutus = dynamic(() => import('../../components/HeroAboutus'), { ssr: false });
+const FoodCategory = dynamic(() => import('../../components/FoodCategory'), { ssr: false });
+const HomePageExperience = dynamic(() => import('../../components/HomePageExperience'), { ssr: false });
+const ChefOnHome = dynamic(() => import('../../components/ChefOnHome'), { ssr: false });
+const SimilarProductsSection = dynamic(() => import('components/SimilarProducts'), { ssr: false });
 
 function HomePage() {
   return (
     <>
       <section className='bg-black px-3 md:px-[135px] flex flex-col justify-evenly md:flex-row  md:items-center py-[50px]'>
-         {/* Heading */}
          <div className='text-white w-full md:w-[50%]'>
           <h1 className='md:text-[32px] text-[24px] font-normal text-[#FF9F0D] font whitespace-nowrap'>
             Its Quick & Amusing!
           </h1>
 
           <h1 className='text-[25px] md:text-[50px] font-bold whitespace-nowrap md:whitespace-normal'>
-
             <span className='text-[#FF9F0D]'>Th</span>e Art of speed food Quality
           </h1>
 
@@ -27,21 +27,20 @@ function HomePage() {
           </p>
 
           <div className='flex flex-col md:flex-row items-center md:items-start'>
-
-          <button className='bg-[#FF9F0D] text-white w-[100px] h-[30px] md:w-[190px] md:h-[60px] rounded-[40px] mt-[32px] hover:bg-yellow-800'>
-            See More
-          </button>
+            <button className='bg-[#FF9F0D] text-white w-[100px] h-[30px] md:w-[190px] md:h-[60px] rounded-[40px] mt-[32px] hover:bg-yellow-800'>
+              See More
+            </button>
           </div>
         </div>
 
-        {/* Image */}
         <div className='mt-[50px] md:mt-0 '>   
             <Image 
-                src="/hero.png"
+                src="/hero.webp"
                 alt='Hero Image'
                 width={700}
                 height={500}
-                className=''
+                priority
+                sizes="(max-width: 768px) 100vw, 700px"
             />
         </div>
     </section>
@@ -55,4 +54,4 @@ function HomePage() {
   )
 }
 
-export default HomePage
+export default HomePage;
